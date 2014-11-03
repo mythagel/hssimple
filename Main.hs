cf327161b7f1cad2c7b4d2d5e2949b730abdd538
Nicholas Gill <mythagel@gmail.com>
2014-11-03 22:54:24 +1100

    Playing

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-import System.Environment
+-- import System.Environment
 {-|
  - Holy crap, compare with
  - https://github.com/mythagel/cxxcam/blob/master/include/Axis.h
@@ -34,6 +34,8 @@ data Units = Metric | Imperial
     deriving (Eq, Read, Show)
 data Plane = XY | ZX | YZ | UV | WU | VW
     deriving (Eq, Read, Show)
+data CoordinateSystem = Active | P1 | P2 | P3 | P4 | P5 | P6 | P7 | P8 | P9
+    deriving (Eq, Read, Show)
 -- ...
 
 g00 = G 0
@@ -51,6 +53,15 @@ g20 = G 20
 g21 = G 21
 g40 = G 40
 g49 = G 49
+g54 = G 54
+g55 = G 55
+g56 = G 56
+g57 = G 57
+g58 = G 58
+g59 = G 59
+g59_1 = G 59.1
+g59_2 = G 59.2
+g59_3 = G 59.3
 -- ...
 
 axis :: Word -> Bool
@@ -74,9 +85,41 @@ offset (J _) = True
 offset (K _) = True
 offset _     = False
 
+planeWord :: Plane -> Word
+planeWord XY = g17
+planeWord ZX = g18
+planeWord YZ = g19
+planeWord UV = g17_1
+planeWord WU = g18_1
+planeWord VW = g19_1
 
+unitsWord :: Units -> Word
+unitsWord Imperial = g20
+unitsWord Metric = g21
+
+coordinateSystemWord :: CoordinateSystem -> Word
+coordinateSystemWord Active = error "Cannot change to Active coordinate system"
+coordinateSystemWord P1 = g54
+coordinateSystemWord P2 = g55
+coordinateSystemWord P3 = g56
+coordinateSystemWord P4 = g57
+coordinateSystemWord P5 = g58
+coordinateSystemWord P6 = g59
+coordinateSystemWord P7 = g59_1
+coordinateSystemWord P8 = g59_2
+coordinateSystemWord P9 = g59_3
+
+preamble :: Plane -> Units -> CoordinateSystem -> [Word]
+preamble plane units cs = [g00, planeWord plane, unitsWord units, g40, g49, coordinateSystemWord cs, G 80]
 
 main :: IO ()
-main = getArgs >>= print . haqify . head
- 
-haqify = (++) "Haq! "
+main = do
+    let x = X 20
+        isLinear = linearAxis x
+    putStrLn $ show isLinear
+    putStrLn $ show $ preamble XY Metric P1
+
+
+
+
+
